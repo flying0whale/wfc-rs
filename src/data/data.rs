5d4867b9a5c1b637a26a2992b6cbe91b0a8ad13f
flying0whale <maks.manin.04@gmail.com>
2024-11-08 23:42:01 +0300

    added variations

--- src/data/data.rs
+++ src/data/data.rs
@@ -3,6 +3,7 @@ pub type Pixel = [u8; 3];
 pub const WHITE: Pixel = [235, 235, 235];
 pub const BLUE:  Pixel = [53,  137, 191];
 pub const BLACK: Pixel = [37,  38,  40];
+pub const RED:   Pixel = [255,  0,  00];
 
 pub const SIDE_UP:    usize = 0;
 pub const SIDE_RIGHT: usize = 1;
@@ -14,12 +15,6 @@ pub struct Cell {
     pub data: [Pixel; 9],
     pub side: [u8; 4]
 }
-
-/*
-    TABLE OF SIDES (b = black; w = white)
-    www: 0
-    wbw: 1
-*/
 
 impl Default for Cell {
     fn default() -> Self {
@@ -36,21 +31,59 @@ impl Cell {
         return Cell { data, side };
     }
 }
-pub const CELLS: [Cell; 19] = [
-    CELL0000, CELL0022, CELL0202,
-    CELL0220, CELL2020, CELL2002,
-    CELL2200, CELL2222,
+pub const CELLS: [Cell; 27] = [
+    // Black cell
+    CELL0000,
+    
+    // Blue cells
+    CELL0022, CELL0202, CELL0220,
+    CELL2020, CELL2002, CELL2200,
+    CELL2222, CELL2220, CELL2202,
+    CELL2022, CELL0222,
+
+    // White cells
     CELL0011, CELL0101, CELL0110,
     CELL1010, CELL1001, CELL1100,
-    CELL1111,
+    CELL1111, CELL1110, CELL1101,
+    CELL1011, CELL0111,
+
+    // Connection cells
     CELL2121, CELL2121_V2,
-    CELL1212, CELL1212_V2
+    CELL1212, CELL1212_V2,
 ];
 
 pub const CELL0000: Cell = Cell {
     side: [0, 0, 0, 0],
     data: [BLACK, BLACK, BLACK,
            BLACK, BLACK, BLACK,
+           BLACK, BLACK, BLACK]
+};
+
+pub const CELL2220: Cell = Cell {
+    side: [2, 2, 2, 0],
+    data: [BLACK, BLUE, BLACK,
+           BLACK, BLUE, BLUE,
+           BLACK, BLUE, BLACK]
+};
+
+pub const CELL0222: Cell = Cell {
+    side: [0, 2, 2, 2],
+    data: [BLACK, BLACK, BLACK,
+           BLUE,  BLUE,  BLUE,
+           BLACK, BLUE,  BLACK]
+};
+
+pub const CELL2022: Cell = Cell {
+    side: [2, 0, 2, 2],
+    data: [BLACK, BLUE, BLACK,
+           BLUE,  BLUE, BLACK,
+           BLACK, BLUE, BLACK]
+};
+
+pub const CELL2202: Cell = Cell {
+    side: [2, 2, 0, 2],
+    data: [BLACK, BLUE,  BLACK,
+           BLUE,  BLUE,  BLUE,
            BLACK, BLACK, BLACK]
 };
 
@@ -178,4 +211,32 @@ pub const CELL2121_V2: Cell = Cell {
     data: [BLACK, BLUE,  BLACK,
            WHITE, WHITE, WHITE,
            BLACK, BLUE,  BLACK]
+};
+
+pub const CELL1110: Cell = Cell {
+    side: [1, 1, 1, 0],
+    data: [BLACK, WHITE, BLACK,
+           BLACK, WHITE, WHITE,
+           BLACK, WHITE, BLACK]
+};
+
+pub const CELL0111: Cell = Cell {
+    side: [0, 1, 1, 1],
+    data: [BLACK, BLACK, BLACK,
+           WHITE, WHITE, WHITE,
+           BLACK, WHITE, BLACK]
+};
+
+pub const CELL1011: Cell = Cell {
+    side: [1, 0, 1, 1],
+    data: [BLACK, WHITE, BLACK,
+           WHITE, WHITE, BLACK,
+           BLACK, WHITE, BLACK]
+};
+
+pub const CELL1101: Cell = Cell {
+    side: [1, 1, 0, 1],
+    data: [BLACK, WHITE, BLACK,
+           WHITE, WHITE, WHITE,
+           BLACK, BLACK, BLACK]
 };
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod data;
 pub mod wfc;
 
 const CELL_SIZE: usize =  3;
-const SIZE:      usize = 50;
+const SIZE:      usize = 20;
 
 fn main() {
     let mut wfc = WFC::new(SIZE, CELL_SIZE);
